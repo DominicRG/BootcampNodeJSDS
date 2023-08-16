8e2a0db71804dac3aa7694f1f22338769f42f039
Dominic Roman <romandominic30@gmail.com>
2023-08-16 12:40:14 -0500

    Clase 21 npm

---
+++ BackEnd con NodeJS/21-Npm/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+console.log(`${process.env.PUERTO}`);
+
+function asdf() {
+  return true;
+}
+
+asdf();
---
+++ BackEnd con NodeJS/ProyectoFinal/src/controllers/users.js
@@ -0,0 +1,12 @@
+const users = require('../models/users');
+
+function userAdd(firstname, lastname, email) {
+  users.add(firstname, lastname, email);
+}
+
+function userShow() {
+  return users.all();
+}
+
+module.exports.userAdd = userAdd;
+module.exports.userShow = userShow;
