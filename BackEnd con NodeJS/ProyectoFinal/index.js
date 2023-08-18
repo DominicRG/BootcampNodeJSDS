9dc9f13b3f4f7c584751061fa995a463553da114
Dominic Roman <romandominic30@gmail.com>
2023-08-17 19:54:55 -0500

    Clase22 Express

--- BackEnd con NodeJS/ProyectoFinal/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const users = require('./controllers/users');
-
-users.userAdd('DANIEL', 'SEGOVIA', 'dani@test.com');
-
-console.log(users.userShow());
-
-console.log(process.env.PORT);
